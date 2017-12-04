409baafcbe6e7f1287074da99e3fa2c151b77c3f
Troels Knak-Nielsen <troelskn@gmail.com>
2017-12-04 12:35:39 +0100

    Tidied up tests

--- spec/htmldiff_spec.rb
+++ spec/htmldiff_spec.rb
@@ -39,20 +39,6 @@ describe "htmldiff" do
     expect(diff).to eq("a <a href=\"#c1\"><a href=\"#c2\"></a> <del class=\"diffmod\">b</del><ins class=\"diffmod\">c</ins>")
   end
 
-  it "changes in properties will render both versions of the start tag, but not end tag" do
-    a = 'a <a href="#c1"></a> b'
-    b = 'a <a href="#c2"></a> c'
-    diff = TestDiff.diff(a, b, false, true)
-    expect(diff).to eq("a <a href=\"#c2\"></a> <del class=\"diffmod\">b</del><ins class=\"diffmod\">c</ins>")
-  end
-
-  it "works when jumping between tags and non tags" do
-    a = 'a <a href="#c1"></a>b<a href="#c1"> c<a href="#c1">e'
-    b = 'a <a href="#c2"></a>c<a href="#c3"> d<a href="#c4">e'
-    diff = TestDiff.diff(a, b, false, true)
-    expect(diff).to eq("a <a href=\"#c2\"></a><del class=\"diffmod\">b</del><ins class=\"diffmod\">c</ins><a href=\"#c3\"> <del class=\"diffmod\">c</del><ins class=\"diffmod\">d</ins><a href=\"#c4\">e")
-  end
-
   it "example from the library" do
     a = '<p>a</p>'
     b = '<p>ab</p><p>c</b>'
@@ -60,11 +46,59 @@ describe "htmldiff" do
     expect(diff).to eq("<p><del class=\"diffmod\">a</del><ins class=\"diffmod\">ab</ins></p><p><ins class=\"diffins\">c</ins></b>")
   end
 
-  it "should reduce consecutive matches" do
-    a = '<p>Han går til samtaler ved en psykiater. Like a boss.</p>'
-    b = '<p>Han drikker stærk spiritus. Like a boss.</p>'
-    expected = '<p>Han <del class="diffmod">går til samtaler ved en psykiater.</del><ins class="diffmod">drikker stærk spiritus.</ins> Like a boss.</p>'
-    diff = TestDiff.diff(a, b, reduce_consecutive: true)
-    expect(diff).to eq(expected)
+  describe "ignore_tags option" do
+    describe "changes in properties should render balanced tags" do
+      it "will render both versions of the start tag, but not end tag when disabled" do
+        a = 'a <a href="#c1"></a> b'
+        b = 'a <a href="#c2"></a> c'
+        expected = 'a <a href="#c1"><a href="#c2"></a> <del class="diffmod">b</del><ins class="diffmod">c</ins>'
+        diff = TestDiff.diff(a, b, ignore_tags: false)
+        expect(diff).to eq(expected)
+      end
+
+      it "will produce valid html when enabled" do
+        a = 'a <a href="#c1"></a> b'
+        b = 'a <a href="#c2"></a> c'
+        expected = 'a <a href="#c2"></a> <del class="diffmod">b</del><ins class="diffmod">c</ins>'
+        diff = TestDiff.diff(a, b, ignore_tags: true)
+        expect(diff).to eq(expected)
+      end
+    end
+
+    describe "when jumping between tags and non tags" do
+      it "will render both versions of the start tag, but not end tag when disabled" do
+        a = 'a <a href="#c1"></a>b<a href="#c1"> c<a href="#c1">e'
+        b = 'a <a href="#c2"></a>c<a href="#c3"> d<a href="#c4">e'
+        expected = 'a <a href="#c1"><a href="#c2"></a><del class="diffmod">b</del><a href="#c1"><ins class="diffmod">c</ins><a href="#c3"> <del class="diffmod">c</del><a href="#c1"><ins class="diffmod">d</ins><a href="#c4">e'
+        diff = TestDiff.diff(a, b, ignore_tags: false)
+        expect(diff).to eq(expected)
+      end
+
+      it "will produce valid html when enabled" do
+        a = 'a <a href="#c1"></a>b<a href="#c1"> c<a href="#c1">e'
+        b = 'a <a href="#c2"></a>c<a href="#c3"> d<a href="#c4">e'
+        expected = 'a <a href="#c2"></a><del class="diffmod">b</del><ins class="diffmod">c</ins><a href="#c3"> <del class="diffmod">c</del><ins class="diffmod">d</ins><a href="#c4">e'
+        diff = TestDiff.diff(a, b, ignore_tags: true)
+        expect(diff).to eq(expected)
+      end
+    end
+  end
+
+  describe "reduce_consecutive option" do
+    it "should diff individual words, when not enabled" do
+      a = '<p>Han går til samtaler ved en psykiater. Like a boss.</p>'
+      b = '<p>Han drikker stærk spiritus. Like a boss.</p>'
+      expected = '<p>Han <del class="diffmod">går</del><ins class="diffmod">drikker</ins> <del class="diffmod">til</del><ins class="diffmod">stærk</ins> <del class="diffmod">samtaler ved en psykiater.</del><ins class="diffmod">spiritus.</ins> Like a boss.</p>'
+      diff = TestDiff.diff(a, b, reduce_consecutive: false)
+      expect(diff).to eq(expected)
+    end
+
+    it "should reduce consecutive matches, when enabled" do
+      a = '<p>Han går til samtaler ved en psykiater. Like a boss.</p>'
+      b = '<p>Han drikker stærk spiritus. Like a boss.</p>'
+      expected = '<p>Han <del class="diffmod">går til samtaler ved en psykiater.</del><ins class="diffmod">drikker stærk spiritus.</ins> Like a boss.</p>'
+      diff = TestDiff.diff(a, b, reduce_consecutive: true)
+      expect(diff).to eq(expected)
+    end
   end
 end
